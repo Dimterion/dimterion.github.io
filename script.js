8ceeca41043aae3df04bb768cdeeb2ee83aaef2f
Dimterion <dimterion59@gmail.com>
2022-12-03 12:10:01 +0100

    Added color schemes, adjusted styles / content.

---
+++ script.js
@@ -0,0 +1,26 @@
+let theme = localStorage.getItem("theme");
+
+if (theme === null) {
+  setTheme("light");
+} else {
+  setTheme(theme);
+}
+
+let themeDots = document.getElementsByClassName("theme-dot");
+
+for (let i = 0; themeDots.length > i; i++) {
+  themeDots[i].addEventListener("click", function () {
+    let mode = this.dataset.mode;
+    setTheme(mode);
+  });
+}
+
+function setTheme(mode) {
+  if (mode === "light") {
+    document.getElementById("theme-style").href = "default.css";
+  } else {
+    document.getElementById("theme-style").href = "blue.css";
+  }
+
+  localStorage.setItem("theme", mode);
+}
